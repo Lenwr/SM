b610c0173eab1fdfbcfd8a2a2f7e6c1064199625
Lenwr <saidabou.lateam@gmail.com>
2023-04-02 15:00:24 +0200

    sm railway

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5610),
+      port: env.int('PGPORT', 6534),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'ksxkPLaKOoiCLhm1FfDH'),
+      password: env('PGPASSWORD', 'CM6ekiaufuA19gwZyV12'),
       ssl: env.bool(true),
     },
   },
